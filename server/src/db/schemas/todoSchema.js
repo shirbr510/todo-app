47d824c10f915af1eb4136153382f947019289b6
Shir Brass <shirbr510@gmail.com>
2017-02-12 18:39:04 +0200

    added timestamp field to mongoose schema

--- server/src/db/schemas/todoSchema.js
+++ server/src/db/schemas/todoSchema.js
@@ -5,7 +5,8 @@ var todoSchema = new Schema({
     _id: Schema.Types.ObjectId,
     todoId:Schema.Types.Number,
     text:Schema.Types.String,
-    isChecked:Schema.Types.Boolean
+    isChecked:Schema.Types.Boolean,
+    timestamp:Schema.Types.Date
 });
 
 export default todoSchema;
